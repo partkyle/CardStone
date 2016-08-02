933e72b105e2598a69906ee79eccd2fe262e3496
Kyle Partridge <partkyle@gmail.com>
2016-08-01 21:31:16 -0700

    adding SoundManager singleton

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundManager : MonoBehaviour
+{
+    public AudioSource efxSource;
+    public AudioSource musicSource;
+    public AudioSource[] sources;
+
+    public static SoundManager instance = null;
+
+    public float lowPitchRange = 0.95f;
+    public float highPitchRange = 1.05f;
+
+    private int sourceIndex = 0;
+
+    // Use this for initialization
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void PlaySingle(AudioClip clip)
+    {
+        AudioSource audioSource = sources[sourceIndex++];
+        audioSource.clip = clip;
+        audioSource.Play();
+
+        sourceIndex %= sources.Length;
+    }
+
+    public void RandomizeSfx(params AudioClip[] clips)
+    {
+        int randomIndex = Random.Range(0, clips.Length);
+        float randomPitch = Random.Range(lowPitchRange, highPitchRange);
+
+        efxSource.pitch = randomPitch;
+        efxSource.clip = clips[randomIndex];
+        efxSource.Play();
+    }
+}
