4c913506a0ed21a28c94ac978a37cdd8645c31ed
Kyle Partridge <partkyle@gmail.com>
2019-04-14 20:14:56 -0700

    minor updates

---
+++ Assets/Scripts/EndTurnButton.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EndTurnButton : MonoBehaviour
+{
+    public Color playerColor;
+    public Color opponentColor;
+
+    Button button;
+    Image image;
+
+    private void Awake() {
+        button = GetComponent<Button>();
+        // this button is handled by an image
+        image = GetComponent<Image>();
+    }
+
+    public void SetTurn(GameManager.Turn turn) {
+        if (turn == GameManager.Turn.Player) {
+            image.color = playerColor;
+        } else {
+            image.color = opponentColor;
+        }
+    }
+}
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -12,8 +12,6 @@ public class GameManager : MonoBehaviour
         Opponent
     };
 
-    Turn currentTurn = Turn.Player;
-
     public class CardPlaceholder
     {
         public string title;
@@ -33,6 +31,53 @@ public class GameManager : MonoBehaviour
     public GameObject opponnentHand;
     public GameObject opponnentTabletop;
 
+    public class Player
+    {
+        public string Tag;
+        public GameObject hand;
+        public GameObject tabletop;
+
+        private List<CardPlaceholder> cards;
+
+        public Player()
+        {
+            InitCards();
+        }
+
+        public void Enable()
+        {
+            tabletop.GetComponent<DropZone>().enabled = true;
+        }
+
+        public void Disable()
+        {
+            tabletop.GetComponent<DropZone>().enabled = false;
+        }
+
+        private void InitCards()
+        {
+            cards = new List<CardPlaceholder>();
+            cards.Add(new CardPlaceholder { title = "Boulderfist Ogre", cost = "6", attack = "6", health = "7" });
+            cards.Add(new CardPlaceholder { title = "Chillwind Yeti", cost = "4", attack = "4", health = "5" });
+            cards.Add(new CardPlaceholder { title = "Yogg Saron, Hope's End", description = "Battlecry: OH SHIT!", cost = "10", attack = "7", health = "5" });
+            cards.Add(new CardPlaceholder { title = "N'zoth", description = "Battlecry: One more time, with feeling.", cost = "10", attack = "5", health = "7" });
+        }
+
+        public CardPlaceholder RandomCard()
+        {
+            CardPlaceholder card = cards[Random.Range(0, cards.Count)];
+            return card;
+        }
+
+        public CardPlaceholder Draw()
+        {
+            return RandomCard();
+        }
+    }
+
+    public Player currentPlayer;
+    public Player otherPlayer;
+
     public void Awake()
     {
         Debug.Log("GameManager::Awake");
@@ -47,93 +92,54 @@ public class GameManager : MonoBehaviour
             return;
         }
 
-        InitCards();
-
-        Turn[] turns = new Turn[] { Turn.Player, Turn.Opponent };
-        foreach (Turn turn in turns)
-        {
-            Hand handObject;
-            if (turn == Turn.Player)
-            {
-                handObject = opponnentHand.GetComponent<Hand>();
-            }
-            else
-            {
-                handObject = playerHand.GetComponent<Hand>();
-            }
-            if (handObject != null)
-            {
-                for (int i = 0; i < 3; i++)
-                {
-                    DrawCardForPlayer(turn);
-                }
-            }
-            else
-            {
-                Debug.LogWarning("Not a hand object");
-            }
-        }
-
+        InitGame();
 
         ResetTurnState();
     }
 
-    private void DrawCardForPlayer(Turn turn)
-    {
-        //handObject.Draw(cards[Random.Range(0, cards.Count - 1)]);
-        CardPlaceholder card = RandomCard();
-        Debug.Log("creating card " + card.title);
-        CreateCardObject(card, turn);
-    }
-
-    public CardPlaceholder RandomCard()
-    {
-        CardPlaceholder card = cards[Random.Range(0, cards.Count)];
-        return card;
-    }
-
-    public void CreateCardObject(CardPlaceholder cardToCreate, Turn player = Turn.Player)
+    public void CreateCardObject(CardPlaceholder cardToCreate, Player player)
     {
         GameObject card = Instantiate(cardPrefab);
         Card c = card.GetComponent<Card>();
 
         c.referenceCard = cardToCreate;
         c.InitCard();
-
-        Transform destination;
-        if (player == Turn.Player)
-        {
-            destination = playerHand.transform;
-            card.tag = "Player";
-        }
-        else
-        {
-            destination = opponnentHand.transform;
-            card.tag = "Opponent";
-        }
+        c.tag = player.Tag;
+        Transform destination = player.hand.transform;
         card.transform.SetParent(destination);
-
-        // FIXME:
-        // not sure why this is needed. This seems to work when the draw card button is clicked.
-        // perhaps it has something to do with the board not being completly loaded, and the size is
-        // off there?
-        card.transform.localScale = Vector3.one;
 
         Debug.Log("created card component " + card);
     }
 
-    private void InitCards()
+    private void InitGame()
     {
-        cards = new List<CardPlaceholder>();
-        cards.Add(new CardPlaceholder { title = "Boulderfist Ogre", cost = "6", attack = "6", health = "7" });
-        cards.Add(new CardPlaceholder { title = "Chillwind Yeti", cost = "4", attack = "4", health = "5" });
-        cards.Add(new CardPlaceholder { title = "Yogg Saron, Hope's End", description = "Battlecry: OH SHIT!", cost = "10", attack = "7", health = "5" });
-        cards.Add(new CardPlaceholder { title = "N'zoth", description = "Battlecry: One more time, with feeling.", cost = "10", attack = "5", health = "7" });
+        currentPlayer = new Player
+        {
+            Tag = "Player",
+            hand = playerHand,
+            tabletop = playerTabletop,
+        };
+
+        otherPlayer = new Player
+        {
+            Tag = "Opponent",
+            hand = opponnentHand,
+            tabletop = opponnentTabletop,
+        };
+
+        CreateCardObject(currentPlayer.Draw(), currentPlayer);
+        CreateCardObject(currentPlayer.Draw(), currentPlayer);
+        CreateCardObject(currentPlayer.Draw(), currentPlayer);
+
+        CreateCardObject(otherPlayer.Draw(), otherPlayer);
+        CreateCardObject(otherPlayer.Draw(), otherPlayer);
+        CreateCardObject(otherPlayer.Draw(), otherPlayer);
+        CreateCardObject(otherPlayer.Draw(), otherPlayer);
     }
 
     public void DropCard(GameObject dropZone, GameObject card, int siblingIndex)
     {
-        GameObject minion = Instantiate<GameObject>(minionPrefab);
+        GameObject minion = Instantiate(minionPrefab);
         Card minionRef = minion.GetComponent<Card>();
         minionRef.referenceCard = card.GetComponent<Card>().referenceCard;
         minionRef.InitCard();
@@ -152,37 +158,35 @@ public class GameManager : MonoBehaviour
 
     public void SwitchTurn()
     {
-        if (currentTurn == Turn.Player)
-        {
-            currentTurn = Turn.Opponent;
-        }
-        else
-        {
-            currentTurn = Turn.Player;
-        }
+        // swap?
+        Player tmp = currentPlayer;
+        currentPlayer = otherPlayer;
+        otherPlayer = tmp;
 
         ResetTurnState();
+        // draw a card
+        CreateCardObject(currentPlayer.Draw(), currentPlayer);
     }
 
     private void ResetTurnState()
     {
-        Debug.Log("setting up turn for " + currentTurn);
+        currentPlayer.Enable();
+        otherPlayer.Disable();
+    }
 
-        if (currentTurn == Turn.Player)
+    public class TurnManager
+    {
+        void Turn()
         {
-            //opponnentHand.GetComponent<Draggable>().enabled = false;
-            opponnentTabletop.GetComponent<DropZone>().enabled = false;
-            //playerHand.GetComponent<Draggable>().enabled = true;
-            playerTabletop.GetComponent<DropZone>().enabled = true;
-        }
-        else
-        {
-            //opponnentHand.GetComponent<Draggable>().enabled = true;
-            opponnentTabletop.GetComponent<DropZone>().enabled = true;
-            //playerHand.GetComponent<Draggable>().enabled = false;
-            playerTabletop.GetComponent<DropZone>().enabled = false;
+            Draw();
+            WaitForMoves();
+            EndTurn();
         }
 
-        DrawCardForPlayer(currentTurn);
+        void Draw() { }
+
+        void WaitForMoves() { }
+
+        void EndTurn() { }
     }
 }
